369325fcd893eee9b3d125dd452488f2ba8f3f82
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-05-04 09:13:22 -0400

    added queries/mutations,resolvers and typeDefs

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    
+  type Book {
+  bookId: String
+  authors: [String]
+  description: String
+  image: String
+  link: String
+  title: String
+}
+
+  type User {
+        _id: ID
+        username: String
+        email: String
+        savedBooks: [Book]
+        bookCount: Int
+    }
+    
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+        user(username: String!): User
+        books(username: String): [Book]
+    }
+    input BookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(savedBook: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+`
+
+module.exports = typeDefs;
